f3852898eb1d635c2ba691d18013b5dc36768c54
Damyan <d.peychev12@gmail.com>
2024-05-13 13:44:08 +0200

    fixing ur stupid mistake

--- GXPEngine/Exit.cs
+++ GXPEngine/Exit.cs
@@ -3,9 +3,21 @@ using GXPEngine.Core;
 
 public class Exit : Sprite
 { 
-    public Exit (Vector2 position) : base ("exit.png", false, false)
+    public Exit (Vector2 position, LevelManager levelManager) : base ("exit.png", false, false)
     {
         this.SetXY(position.x, position.y);
         this.SetOrigin (this.width /2, this.height / 2);
+    }
+
+    void Update ()
+    {
+        DetectCollision ();
+    }
+
+    void DetectCollision ()
+    {
+        Ball ball = ((MyGame)game).GetPlayer();
+
+
     }
 }
--- GXPEngine/Levels/LevelObjectParams.cs
+++ GXPEngine/Levels/LevelObjectParams.cs
@@ -13,11 +13,10 @@ public class LevelObjectParams
     public float bounciness;
     public int mode;
     public bool isStatic;
-    public GameSettings gameSettings;
 
     public string imageName;
 
-    public LevelObjectParams(LevelObjectType type, string imageName, Vector2 position, Vector2 position2,float width, float height, float rotation, float density, float bounciness, int mode, bool isStatic, GameSettings gameSettings)
+    public LevelObjectParams(LevelObjectType type, string imageName, Vector2 position, Vector2 position2,float width, float height, float rotation, float density, float bounciness, int mode, bool isStatic)
     {
         this.type = type;
         this.imageName = imageName;
@@ -30,7 +29,6 @@ public class LevelObjectParams
         this.bounciness = bounciness;
         this.mode = mode;
         this.isStatic = isStatic;
-        this.gameSettings = gameSettings;
     }
 }
 
